0ee3d363738a218a468d8ae85bcaf11eef55a826
Wesley Lima <wesleylima2209@gmail.com>
2020-08-18 06:50:14 -0300

    Adicionando cidade e estado

--- src/main/java/com/wesleylima/estudo/EstudoApplication.java
+++ src/main/java/com/wesleylima/estudo/EstudoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wesleylima.estudo.domain.Categoria;
+import com.wesleylima.estudo.domain.Cidade;
+import com.wesleylima.estudo.domain.Estado;
 import com.wesleylima.estudo.domain.Produto;
 import com.wesleylima.estudo.repository.CategoriaRepository;
+import com.wesleylima.estudo.repository.CidadeRepository;
+import com.wesleylima.estudo.repository.EstadoRepository;
 import com.wesleylima.estudo.repository.ProdutoRepository;
 
 @SpringBootApplication
@@ -18,6 +22,10 @@ public class EstudoApplication implements CommandLineRunner {
 	@Autowired private CategoriaRepository categoriaRepository;
 	
 	@Autowired private ProdutoRepository produtoRepository;
+	
+	@Autowired private CidadeRepository cidadeRepository;
+	
+	@Autowired private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
@@ -43,6 +51,19 @@ public class EstudoApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "São Paulo");
+		Estado est2 = new Estado(null, "Minas Gerais");
+		
+		Cidade c1 = new Cidade(null, "Campinas", est1);
+		Cidade c2 = new Cidade(null, "Uberlândia", est2);
+		Cidade c3 = new Cidade(null, "São Paulo", est1);
+		
+		est1.getCidades().add(c2);
+		est2.getCidades().addAll(Arrays.asList(c1,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
