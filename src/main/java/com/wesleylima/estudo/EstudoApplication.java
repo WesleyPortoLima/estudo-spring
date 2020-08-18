af905b8e5be2425809dbc0b1b4db9f958e9ad47a
Wesley Lima <wesleylima2209@gmail.com>
2020-08-18 05:53:41 -0300

    Produto e associações

--- src/main/java/com/wesleylima/estudo/EstudoApplication.java
+++ src/main/java/com/wesleylima/estudo/EstudoApplication.java
@@ -8,12 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wesleylima.estudo.domain.Categoria;
+import com.wesleylima.estudo.domain.Produto;
 import com.wesleylima.estudo.repository.CategoriaRepository;
+import com.wesleylima.estudo.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class EstudoApplication implements CommandLineRunner {
 
 	@Autowired private CategoriaRepository categoriaRepository;
+	
+	@Autowired private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
@@ -25,7 +29,20 @@ public class EstudoApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
