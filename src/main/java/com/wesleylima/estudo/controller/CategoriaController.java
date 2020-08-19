a61eaa58d75cb54dd6129c21731bcf91bae0cff9
Wesley Lima <wesleylima2209@gmail.com>
2020-08-19 05:33:32 -0300

    Adicionando findAll com paginação

--- src/main/java/com/wesleylima/estudo/controller/CategoriaController.java
+++ src/main/java/com/wesleylima/estudo/controller/CategoriaController.java
@@ -2,15 +2,16 @@ package com.wesleylima.estudo.controller;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -60,8 +61,19 @@ public class CategoriaController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
-		List<Categoria> categoria = categoriaService.findAll();
-		List<CategoriaDTO> listDto = categoria.stream().map(cat -> new CategoriaDTO(cat)).collect(Collectors.toList());
+		List<Categoria> categorias = categoriaService.findAll();
+		List<CategoriaDTO> listDto = categorias.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	} 
+	
+	@RequestMapping(value = "/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "sortBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = categorias.map(cat -> new CategoriaDTO(cat));
 		return ResponseEntity.ok().body(listDto);
 	} 
 }
--- src/main/java/com/wesleylima/estudo/service/CategoriaService.java
+++ src/main/java/com/wesleylima/estudo/service/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wesleylima.estudo.domain.Categoria;
@@ -44,5 +47,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(final Integer page, final Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return categoriaRepository.findAll(pageRequest);
 	}
 }
