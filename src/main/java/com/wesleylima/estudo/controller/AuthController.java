da4413b0d3419391604323dc4b75b8e37306a8b1
Wesley Lima <wesleylima2209@gmail.com>
2020-08-22 13:56:20 -0300

    Adicionando refresh token

---
+++ src/main/java/com/wesleylima/estudo/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.wesleylima.estudo.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesleylima.estudo.security.JWTUtil;
+import com.wesleylima.estudo.security.UserSS;
+import com.wesleylima.estudo.service.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+	
+	@Autowired private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+	UserSS user = UserService.authenticated();
+	String token = jwtUtil.generateToken(user.getUsername());
+	response.addHeader("Authorization", "Bearer " + token);
+	return ResponseEntity.noContent().build();
+	}
+}
